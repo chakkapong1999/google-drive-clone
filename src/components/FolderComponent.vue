ef421a61dc4dac65b62eb260fbb74d719c079faf
Chakkapong <chakkapong1999@gmail.com>
2023-05-27 20:37:28 +0700

    update ui

---
+++ src/components/FolderComponent.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="folder-box" @contextmenu.prevent="handleRightClick($event)">
+      <b-icon-folder class="icon"></b-icon-folder>
+      {{ markFields(folder.folderName) }}
+      <div></div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'FolderComponent',
+  props: {
+    folder: Object
+  },
+  data () {
+    return {
+      isRightClick: false
+    }
+  },
+  methods: {
+    markFields (text) {
+      const MAX_LENGTH = 12
+      if (text.length > MAX_LENGTH) {
+        return text.substring(0, MAX_LENGTH) + '...'
+      }
+      return text
+    },
+    handleRightClick (event) {
+      this.isRightClick = true
+      console.log(event)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.folder-box {
+  border: 1px solid rgb(161, 156, 156);
+  border-radius: 5px;
+  width: 10rem;
+  height: 2.5rem;
+  text-align: center;
+  word-break: break-all;
+  display: flex;
+  justify-content: space-evenly;
+  place-items: center;
+  cursor: pointer;
+  /* padding: 0 0 0 1rem; */
+}
+
+.folder-box:hover {
+  background-color: rgb(223, 221, 221);
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,144 @@
 <template>
-  <div class="home">
-    Home
+  <div id="home">
+    <div class="button mt-5 mb-5">
+      <button class="btn btn-warning">UPLOAD FILE</button>
+      <button class="btn btn-primary" @click="isShowModal = true">CREATE FOLDER</button>
+    </div>
+    <b-modal centered v-model="isShowModal" title="New Folder" id="create-modal">
+      <div class="bubble">
+        <input type="text" class="form-control" placeholder="Folder Name" v-model="folder.folderName" />
+        <b-form-checkbox v-model="showPassword" :value="true" :unchecked-value="false">
+          Password
+        </b-form-checkbox>
+        <input v-if="showPassword" type="password" class="form-control" placeholder="Password" v-model="folder.folderPassword" />
+      </div>
+
+      <template #modal-footer>
+        <button class="btn btn-secondary" @click="cancel()">
+          CANCEL
+        </button>
+        <button class="btn btn-success" @click="createFolder()">
+          CREATE
+        </button>
+      </template>
+    </b-modal>
+    <hr>
+    <div class="folder" v-if="folderProps.length > 0">
+      <template v-for="(item, index) of folderProps">
+        <FolderComponent :folder="item" :key="index" />
+      </template>
+    </div>
   </div>
 </template>
 
 <script>
-
+import FolderComponent from '@/components/FolderComponent.vue'
 export default {
-  name: 'HomeView'
+  name: 'HomeView',
+  components: {
+    FolderComponent
+  },
+  data () {
+    return {
+      isShowModal: false,
+      showPassword: false,
+      folder: {
+        folderName: '',
+        folderPassword: '',
+        folderStatus: 'active',
+        folderOwner: ''
+      },
+      folderProps: []
+    }
+  },
+  methods: {
+    cancel () {
+      this.clearTempData()
+      this.$bvModal.hide('create-modal')
+    },
+    createFolder () {
+      const formData = new FormData()
+
+      formData.append('files', 'filesArray')
+      formData.append('payload',
+        JSON.stringify(
+          {
+            folderName: this.folder.folderName,
+            folderOwner: this.folder.folderOwner,
+            folderStatus: this.folder.folderStatus,
+            folderPassword: this.folder.folderPassword
+          }
+        )
+
+      )
+
+      // console.log(this.folderName)
+      // console.log(this.folderPassword)
+      // console.log(this.folderStatus)
+      // console.log(this.folderOwner)
+      console.log(formData)
+
+      const folder = {
+        folderName: this.folder.folderName,
+        folderOwner: this.folder.folderOwner,
+        folderStatus: this.folder.folderStatus,
+        folderPassword: this.folder.folderPassword
+      }
+      this.folderProps.push(folder)
+      this.clearTempData()
+
+      this.$bvModal.hide('create-modal')
+    },
+    clearTempData () {
+      this.folder = {
+        folderName: '',
+        folderOwner: '',
+        folderStatus: 'active',
+        folderPassword: ''
+      }
+      this.showPassword = false
+    }
+  }
 }
 </script>
+
+<style scoped>
+#home {
+  height: 100vh;
+}
+
+.button {
+  display: flex;
+  justify-content: center;
+  gap: 5rem 1rem;
+}
+
+.bubble {
+  display: grid;
+  word-break: break-all;
+  row-gap: 18px;
+  padding: 1rem;
+  width: 100%;
+}
+
+.ok-button {
+  height: 42px;
+  /* margin-top: 0.5rem; */
+  margin-bottom: 4px;
+  color: white;
+  background-color: #4ba659;
+  border-color: #4ba659;
+  width: 100%;
+  display: grid;
+  justify-self: center;
+  text-align: center;
+}
+
+.folder {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 2rem;
+  padding: 1rem 5rem 2rem 5rem;
+}
+</style>
